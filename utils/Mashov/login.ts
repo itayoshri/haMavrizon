bb3a7dc5f49c3a3455e2f1e24b3fd271b224ca95
Itay Oshri <itay.oshri@gmail.com>
2022-10-02 16:05:00 +0300

    FEATURE: Function

---
+++ utils/Mashov/login.ts
@@ -0,0 +1,47 @@
+import axios from 'axios'
+
+const MASHOV_LOGIN_URL = 'https://web.mashov.info/api/login'
+
+export default async function MashovLogin({
+  password,
+  semel,
+  username,
+}: {
+  password: string
+  semel: number
+  username: string
+}) {
+  const data = JSON.stringify({
+    appName: 'info.mashov.students',
+    password: password,
+    semel: Number(semel),
+    username: username,
+    year: 2023,
+  })
+
+  const config = {
+    method: 'post',
+    url: MASHOV_LOGIN_URL,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: data,
+  }
+
+  const response = await await axios(config)
+
+  // Auth cookie
+  const auth = response.headers['set-cookie'][1]
+  const token = auth.slice(0, 672)
+  const domain = 'domain=web.mashov.info'
+  const path = '/'
+
+  // Student info requests
+  const studentId = response.data.userId
+  const STUDENT_URL = `https://web.mashov.info/api/students/${studentId}`
+
+  return {
+    authCookie: `${token};domain=${domain};path=/;`,
+    studentId: studentId,
+  }
+}
