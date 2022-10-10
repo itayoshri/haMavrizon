e30d143cb70542eb36f9e787d13e0440aa1c4dff
Itay Oshri <itay.oshri@gmail.com>
2022-10-10 17:02:18 +0300

    FEATURE: Interface

--- Interfaces/index.ts
+++ Interfaces/index.ts
@@ -1,4 +1,10 @@
 export interface IStudyGroup {
   name: string
   groupId: number
+  freeAbsences: number
+  freeAnnualAbsences: number
+  semesterHours: number
+  lessonsCount: number
+  weeklyHours: number
+  absenceCounter: number
 }
