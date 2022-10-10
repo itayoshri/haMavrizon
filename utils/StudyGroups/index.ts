0299c718da15b673b05fdf6455e7793ab49f6971
Itay Oshri <itay.oshri@gmail.com>
2022-10-10 16:47:33 +0300

    FEATURE: Absence, semester hours etc.

--- utils/StudyGroups/index.ts
+++ utils/StudyGroups/index.ts
@@ -6,18 +6,49 @@ import {
   eventCodes,
 } from '../../Interfaces/Mashov'
 
-class StudyGroup implements IStudyGroup {
+const WEEKS_OF_STUDY = 89 / 5 // 89 is the number of the study days between 01/09/22 and 26/01/23, 5 days per study week
+const ABS_CONST = 1.17647 // Evaluation of f(n) = n + f(0.15n)
+
+class StudyGroup {
   readonly name: string
   readonly groupId: number
 
-  public lessonsCount = 0
-  public semesterHours = 0
-  public weeklyHours = 0
-  public absenceCounter = 0
+  private freeAbsences = 0
+  private semesterHours = 0
+  private lessonsCount = 0
+  private weeklyHours = 0
+  private absenceCounter = 0
 
   constructor({ name, groupId }: { name: string; groupId: number }) {
     this.name = name
     this.groupId = groupId
+  }
+
+  public addAbsence() {
+    this.absenceCounter++
+    this.freeAbsences = StudyGroup.AbsCalc(
+      this.lessonsCount,
+      this.absenceCounter
+    )
+  }
+
+  public updateHours(lessonsCount: number, weeklyHours: number) {
+    this.lessonsCount = lessonsCount
+    this.weeklyHours = weeklyHours
+    this.semesterHours = StudyGroup.calcSemesterHours(weeklyHours)
+  }
+
+  public static AbsCalc(lessonsCount: number, absenceCounter: number) {
+    const dryCalcAbs = lessonsCount * 0.15 - absenceCounter
+    return dryCalcAbs * ABS_CONST
+  }
+
+  public static AnnualAbsCalc(semesterHours: number, absenceCounter: number) {
+    return semesterHours * 0.15 - absenceCounter
+  }
+
+  public static calcSemesterHours(weeklyHours: number) {
+    return weeklyHours * WEEKS_OF_STUDY
   }
 }
 
@@ -41,15 +72,14 @@ export class StudyGroupsBuilder {
     for (const studyGroup of lessonCounter) {
       const sg = this.studyGroups.get(studyGroup.groupId)
       if (sg != undefined) {
-        sg.weeklyHours = studyGroup.weeklyHours
-        sg.lessonsCount = studyGroup.lessonsCount
+        sg.updateHours(studyGroup.lessonsCount, studyGroup.weeklyHours)
       }
     }
 
     for (const event of behaveEvents) {
       if (event.eventCode == eventCodes.ABSENCE) {
         const sg = this.studyGroups.get(event.groupId)
-        if (sg != undefined) sg.absenceCounter++
+        if (sg != undefined) sg.addAbsence()
       }
     }
   }
