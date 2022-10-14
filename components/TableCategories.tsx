0ebd884ab34f02e88cd233a8c9028618b7d0d92d
Itay Oshri <itay.oshri@gmail.com>
2022-10-15 01:03:08 +0300

    FEATURE: Table categories

---
+++ components/TableCategories.tsx
@@ -0,0 +1,19 @@
+const SUBJECT = 'מקצוע'
+const SEMESTERIAL = 'מחצית'
+const CURRENTLY = 'כעת'
+
+export default function TableCategories() {
+  return (
+    <div className="flex justify-between w-full pr-4 font-bold">
+      <h1>{SUBJECT}</h1>
+      <div className="flex w-[120px] pl-2">
+        <div className="w-1/2 text-center">
+          <a>{CURRENTLY}</a>
+        </div>
+        <div className="w-1/2 text-center">
+          <a>{SEMESTERIAL}</a>
+        </div>
+      </div>
+    </div>
+  )
+}
