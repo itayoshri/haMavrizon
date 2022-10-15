fded9c331ad1bca4156b8cf26e98d26a5523b27d
Itay Oshri <itay.oshri@gmail.com>
2022-10-15 15:13:31 +0300

    FEATURE: Input

---
+++ components/Forms/Input.tsx
@@ -0,0 +1,15 @@
+export interface InputProps {
+  hint: string
+  onChange(newInput: any): unknown
+  password?: boolean
+}
+
+export default function Input({ hint, onChange, password }: InputProps) {
+  return (
+    <input
+      placeholder="סמל בית ספר וזה"
+      className="bg-zinc-200 p-2 px-4 rounded-lg placeholder:text-black text-lg text-right"
+      onChange={(e) => onChange(Number(e.target.value))}
+    />
+  )
+}
