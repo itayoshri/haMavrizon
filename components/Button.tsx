4487a729c87595f1a12ebf00491f6124016a4242
Itay Oshri <itay.oshri@gmail.com>
2022-10-15 14:59:32 +0300

    FEATURE: Button

---
+++ components/Button.tsx
@@ -0,0 +1,19 @@
+import { Wrapper } from './types'
+
+export interface ButtonProps {
+  onClick(): unknown
+}
+
+export default function Button({
+  onClick = () => {},
+  children,
+}: ButtonProps & Wrapper) {
+  return (
+    <button
+      className={`bg-sky-500 text-white font-bold py-3 px-6 rounded-xl`}
+      onClick={() => onClick()}
+    >
+      {children}
+    </button>
+  )
+}
