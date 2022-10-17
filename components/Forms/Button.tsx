3bb16941d19c618299629112dff82fccc1381b70
Itay Oshri <itay.oshri@gmail.com>
2022-10-18 02:28:10 +0300

    FEATURE: ClassName on button

--- components/Forms/Button.tsx
+++ components/Forms/Button.tsx
@@ -2,15 +2,17 @@ import { Wrapper } from '../types'
 
 export interface ButtonProps {
   onClick(): unknown
+  className?: string
 }
 
 export default function Button({
   onClick = () => {},
   children,
+  className = '',
 }: ButtonProps & Wrapper) {
   return (
     <button
-      className={`bg-[#106030] font-mashov text-sm text-white w-full py-3 px-6 rounded-full`}
+      className={`bg-[#106030] font-mashov text-sm text-white w-full py-3 px-6 rounded-full shadow-md ${className}`}
       onClick={() => onClick()}
     >
       {children}
