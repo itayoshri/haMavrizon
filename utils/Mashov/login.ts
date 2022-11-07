4cfade7237928d0a9647de61767bf1e7c7770ca9
Itay Oshri <itay.oshri@gmail.com>
2022-11-07 20:04:36 +0200

    FEATURE: ENV

--- utils/Mashov/login.ts
+++ utils/Mashov/login.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 import { IMashovLoginRes } from '../../Interfaces/Mashov'
 
-const MASHOV_LOGIN_URL = 'https://web.mashov.info/api/login'
-const MASHOV_OTP_LOGIN_URL = 'https://web.mashov.info/api/user/otp'
+const MASHOV_LOGIN_URL = process.env.LOGIN_URL as string //'https://ha-mavrizon-server.vercel.app/api'
+const MASHOV_OTP_LOGIN_URL = process.env.OTP_LOGIN_URL as string //'https://web.mashov.info/api/user/otp'
 
 export default async function MashovLogin({
   password,
