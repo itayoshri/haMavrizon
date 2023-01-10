5c0ecd33de7ec807388e7741c3a6591183750310
Itay Oshri <itay.oshri@gmail.com>
2023-01-10 11:55:20 +0200

    REFACTOR: Add icons to subject's full info

--- components/Subject/FullInfo/FullInfo.tsx
+++ components/Subject/FullInfo/FullInfo.tsx
@@ -1,4 +1,5 @@
 import { IFrontStudyGroup } from '../../../Interfaces'
+import { FreeAbsence, Later, Now, Out } from '../../Icons'
 import InfoLine from './InfoLine'
 
 const LESSONS_COUNT = 'מספר השיעורים שהתקיימו'
@@ -16,12 +17,34 @@ export default function FullInfo({
 }: IFrontStudyGroup) {
   return (
     <div className="flex flex-col">
-      <InfoLine title={LESSONS_COUNT} info={lessonsCount} />
-      <InfoLine title={SEMESTERIAL_LESSONS} info={semesterHours} />
-      <InfoLine title={NUM_OF_ABSENCES} info={absenceCounter} />
+      <InfoLine
+        title={LESSONS_COUNT}
+        info={lessonsCount}
+        icon={<Now width={20} />}
+      />
+      <InfoLine
+        title={SEMESTERIAL_LESSONS}
+        info={semesterHours}
+        icon={<Later width={20} />}
+        predicted
+      />
+      <InfoLine
+        title={NUM_OF_ABSENCES}
+        info={absenceCounter}
+        icon={<Out width={20} />}
+      />
 
-      <InfoLine title={FREE_ABSENCES} info={freeAbsences} />
-      <InfoLine title={SEMESTERIAL_FREE_ABSENCES} info={freeAnnualAbsences} />
+      <InfoLine
+        title={FREE_ABSENCES}
+        info={freeAbsences}
+        icon={<FreeAbsence width={20} />}
+      />
+      <InfoLine
+        title={SEMESTERIAL_FREE_ABSENCES}
+        info={freeAnnualAbsences}
+        icon={<FreeAbsence width={20} />}
+        predicted
+      />
     </div>
   )
 }
--- components/Subject/FullInfo/InfoLine.tsx
+++ components/Subject/FullInfo/InfoLine.tsx
@@ -1,12 +1,26 @@
 export interface InfoLineProps {
   title: string
+  icon?: JSX.Element
   info: string | number
+  predicted?: boolean
 }
 
-export default function InfoLine({ title, info }: InfoLineProps) {
+export default function InfoLine({
+  title,
+  info,
+  icon,
+  predicted = false,
+}: InfoLineProps) {
   return (
-    <div className="flex justify-between items-center">
-      <h1>{title}</h1>
+    <div
+      className={`flex justify-between items-center ${
+        predicted ? 'text-sky-500' : ''
+      }`}
+    >
+      <div className="flex gap-2">
+        {icon ? icon : <div className="w-5" />}
+        <h1>{title}</h1>
+      </div>
       <a>{info}</a>
     </div>
   )
