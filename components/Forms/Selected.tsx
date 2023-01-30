76637b60d9258a9776a35398921dd17116201b01
Itay Oshri <itay.oshri@gmail.com>
2023-01-30 20:20:46 +0200

    REFACTOR: Selected

--- components/Forms/Selected.tsx
+++ components/Forms/Selected.tsx
@@ -9,7 +9,7 @@ export default function Selected({ selected, setSelected }: SelectedProps) {
   return (
     <button
       className={`rounded-md p-1 w-7 h-7 ${
-        selected ? 'bg-blue-500' : 'bg-white border-[1px] border-gray-400'
+        selected ? 'bg-blue-500' : 'bg-white border-[2px] border-gray-400'
       }`}
       onClick={() => setSelected()}
     >
