68d8ff943bfa4bcdaab4d1446e59d33868bd9b3c
Itay Oshri <itay.oshri@gmail.com>
2023-01-30 20:05:58 +0200

    FEATURE: Selected

---
+++ components/Forms/Selected.tsx
@@ -0,0 +1,19 @@
+import { Check } from '../Icons'
+
+export interface SelectedProps {
+  selected: boolean
+  setSelected(): unknown
+}
+
+export default function Selected({ selected, setSelected }: SelectedProps) {
+  return (
+    <button
+      className={`rounded-md p-1 w-7 h-7 ${
+        selected ? 'bg-blue-500' : 'bg-white border-[1px] border-gray-400'
+      }`}
+      onClick={() => setSelected()}
+    >
+      {selected ? <Check className="text-white" /> : null}
+    </button>
+  )
+}
