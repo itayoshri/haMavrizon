bb89dadea712ab1a34786932a9c93feaedefa440
Itay Oshri <itay.oshri@gmail.com>
2023-01-30 18:46:21 +0200

    FEATURE: Signle grade

---
+++ components/Subject/grade.tsx
@@ -0,0 +1,35 @@
+import { IGrade } from '../../utils/StudyGroups/grades'
+import AbsElipse from './AbsElipse'
+
+export interface GradeProps {
+  setSelected(bool): unknown
+}
+
+export default function Grade({
+  setSelected,
+  title,
+  grade,
+  eventDate,
+  gradeType,
+  selected,
+}: GradeProps & IGrade) {
+  return (
+    <div className="flex w-full justify-between py-3 px-5 border-b-[1px] dark:text-white border-zinc-200 dark:border-zinc-800">
+      <div className="flex flex-col truncate">
+        <span className="truncate text-ellipsis">
+          <a className="font-medium">{title}</a>
+          <a className="text-xs text-gray-400 mr-1">({gradeType})</a>
+        </span>
+        <a className="text-sm">07/01/2023</a>
+      </div>
+      <div className="">
+        <AbsElipse
+          label={grade}
+          lessonsCount={100}
+          absenceCounter={grade}
+          type="grade"
+        />
+      </div>
+    </div>
+  )
+}
