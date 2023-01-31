13cbfd0f3276444f2204f87470532ee85b695431
Itay Oshri <itay.oshri@gmail.com>
2023-01-31 14:44:01 +0200

    REFACTOR: Padding

--- components/Views/AverageView.tsx
+++ components/Views/AverageView.tsx
@@ -8,7 +8,7 @@ export interface AverageViewProps {
 
 export default function AverageView({ label, average }: AverageViewProps) {
   return (
-    <div className="flex font-medium px-4 justify-between items-center text-blue-500">
+    <div className="flex font-medium px-5 justify-between items-center text-blue-500">
       <div className="flex gap-1 items-center">
         <Calculator className="text-blue-500" width={26} />
         <a>{label}</a>
