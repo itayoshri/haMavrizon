b9fc3a9c5e195aaa8dcfd79ed1a30dde0164b4b5
Itay Oshri <itay.oshri@gmail.com>
2023-01-31 14:54:31 +0200

    DARKMODE: Selected

--- components/Forms/Selected.tsx
+++ components/Forms/Selected.tsx
@@ -9,7 +9,7 @@ export default function Selected({ selected, setSelected }: SelectedProps) {
   return (
     <button
       className={`rounded-md shrink-0 p-1 w-7 h-7 ${
-        selected ? 'bg-blue-500' : 'bg-white border-[2px] border-gray-400'
+        selected ? 'bg-blue-500' : 'border-[2px] border-gray-400'
       }`}
       onClick={() => setSelected()}
     >
