b52ddd31a772d9561c4d8e7f0f2bf31e588dfe1a
Itay Oshri <itay.oshri@gmail.com>
2023-12-24 16:54:39 +0200

    FIX: Remove unused code

--- components/Views/DashboardView.tsx
+++ components/Views/DashboardView.tsx
@@ -8,40 +8,20 @@ import { useGradesProvider, useModesProvider } from '../../contexts'
 import Navbar from '../Navbar'
 import Footer from '../Footer'
 import SubjectAbsences from '../Subject/absences'
-
-/*const POPUP = {
-  title: 'חדש!',
-  description: 'ניתן לקבל כעת מידע מורחב בלחיצה על העיגולים',
-  buttonLabel: 'הבנתי',
-  image: elipsesfullinfo_sample,
-}*/
 interface DashboardViewProps {
   showed: boolean
   setShowed(val: any): unknown
 }
 
-export default function DashboardView({
-  showed,
-  setShowed,
-}: DashboardViewProps) {
-  const [opened, setOpened] = useState(!showed)
-  const { selectedMode } = useModesProvider()
-  const { gradesStudyGroups, setGradesStudyGroups, absencesStudyGroups } =
-    useGradesProvider()
+const ALL_SUBJECTS = 'כל המקצועות'
 
-  const setSelected = useCallback(
-    (index: number) => {
-      gradesStudyGroups[index].selected = !gradesStudyGroups[index].selected
-      setGradesStudyGroups((prev) => {
-        return [...prev]
-      })
-    },
-    [gradesStudyGroups, setGradesStudyGroups]
-  )
-
+export default function DashboardView({ showed }: DashboardViewProps) {
+  const { absencesStudyGroups } = useGradesProvider()
   return (
-    <div className="flex flex-col pt-20 px-4 h-full w-screen items-start sm:w-full m-0 p-0 justify-center">
-      <h1 className="text-black dark:text-white">כל המקצועות</h1>
+    <div className="flex flex-col pt-4 px-4 h-full w-screen gap-4 items-start sm:w-full m-0 p-0 justify-center">
+      <h1 className="text-black dark:text-white text-2xl font-semibold">
+        {ALL_SUBJECTS}
+      </h1>
       <div className="flex flex-col w-full gap-4">
         {absencesStudyGroups.map((subject, key) => (
           <SubjectAbsences {...subject} key={key} />
