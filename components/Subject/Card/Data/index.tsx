40a04e1996be39567360db1885148502bfed0ff4
Itay Oshri <itay.oshri@gmail.com>
2024-01-26 19:23:30 +0200

    REFACTOR: Card Data

--- components/Subject/Card/Data/index.tsx
+++ components/Subject/Card/Data/index.tsx
@@ -6,9 +6,9 @@ import DataRow from './Row'
 
 const LESSONS_COUNT = 'שיעורים שהתקיימו'
 const SEMESTERIAL_LESSONS = 'שיעורים במחצית (משוער)'
-const NUM_OF_ABSENCES = 'מספר ההעדרויות'
-const FREE_ABSENCES = 'העדרויות ללא הורדה בציון שנותרו כעת'
-const SEMESTERIAL_FREE_ABSENCES = 'העדרויות ללא הורדה בציון שנותרו במחצית'
+const FREE_ABSENCES = 'העדרויות ללא הורדה בציון שנותרו'
+const NOW = 'כעת'
+const SEMESTER = 'מחצית'
 
 export default function CardData({
   freeAbsences,
@@ -24,17 +24,17 @@ export default function CardData({
         <DataRow title={SEMESTERIAL_LESSONS} info={semesterHours} />
       </div>
       <div className="flex flex-col justify-center items-center gap-3">
-        <h1 className="font-semibold">העדרויות ללא הורדה בציון שנותרו</h1>
+        <h1 className="font-semibold">{FREE_ABSENCES}</h1>
         <div className="flex gap-4 items-center">
           <TitledCircle
-            title="כעת"
+            title={NOW}
             label={freeAnnualAbsences}
             lessonsCount={semesterHours}
             absenceCounter={absenceCounter}
           />
           <div className="bg-black w-[2px] h-8" />
           <TitledCircle
-            title="מחצית"
+            title={SEMESTER}
             label={freeAbsences}
             lessonsCount={lessonsCount}
             absenceCounter={absenceCounter}
